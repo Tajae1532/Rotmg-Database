11c7209b22887d72bfaa3f2b70ea14ddd8d53341
Tajae1532 <91098649+Tajae1532@users.noreply.github.com>
2021-10-01 22:01:43 -0400

    Add files via upload

---
+++ MySQlDataReader.java
@@ -0,0 +1,15 @@
+package rotmgprod;
+
+public class MySQlDataReader {
+
+	public static final boolean HasRows = false;
+
+	public String GetString(String string) {
+		return null;
+	}
+
+	public void Read() {
+		
+	}
+
+}
---
+++ MySqlCommand.java
@@ -0,0 +1,11 @@
+package rotmgprod;
+
+public class MySqlCommand {
+
+	public static final String Parameters = null;
+	public String CommandText;
+	public MySQlDataReader ExecuteReader() {
+		return null;
+	}
+
+}
