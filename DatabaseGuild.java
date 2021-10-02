8fda5ee4ba0169e1bbd5b9db8713cc9f9f9eaff9
Tajae1532 <91098649+Tajae1532@users.noreply.github.com>
2021-10-01 22:02:14 -0400

    Update DatabaseGuild.java
    Changes

--- DatabaseGuild.java
+++ DatabaseGuild.java
@@ -1,6 +1,7 @@
 package rotmgprod;
 
 import java.lang.System;
+import java.util.List;
 
 public class DatabaseGuild {
 	public final String GetGuildName(long id) {
@@ -16,6 +17,11 @@ public class DatabaseGuild {
 		return rdr.GetString("name");
 	}
 
+	private MySqlCommand CreateQuery() {
+	
+		return null;
+	}
+
 	public final int GetGuildId(String name) {
 		MySqlCommand cmd = CreateQuery();
 		cmd.CommandText = "SELECT id FROM guilds WHERE name=@name;";
@@ -29,7 +35,7 @@ public class DatabaseGuild {
 		return rdr.GetInt32("id");
 	}
 
-     public final List<GuildStruct> GetGuilds() {
+     public final GuildStruct[] GetGuilds() {
     	 MySqlCommand cmd = CreateQuery();
     	 cmd.CommandText = "SELECT * FROM guilds";
     	 List<GuildStruct> guilds = new List<GuildStruct>();
@@ -43,16 +49,62 @@ public class DatabaseGuild {
 			Object idUnknown;
 			Object nameUnknown;
 			Object Name;
+			Object Level;
+			Object Members;
+			Object GuildFame;
+			Object guildFameUnknown;
+			Object totalGuildFameUnknown;
+			Object TotalGuildFame;
+			Object levelUnknown;
+			Object Unknown;
+			Object membersUnknown;
 			guilds.Add(new GuildStruct [] {
     				 Id=rdr.GetInt32(idUnknown,
     	                        Name=rdr.GetString(nameUnknown,
     	                        Level=rdr.GetInt32(levelUnknown,
-    	                        Members=rdr.GetString(membersUnknown.Split(,Unknown,
+    	                        Members=rdr.GetString(membersUnknown.Split(),Unknown,
     	                        GuildFame=rdr.GetInt32(guildFameUnknown,
-    	                        TotalGuildFame=rdr.GetInt32(totalGuildFameUnknown});
+    	                        TotalGuildFame=rdr.GetInt32(totalGuildFameUnknown); {
     		 }
     	 return guilds;
     	 }
+     }
+			
+			public final GuildStruct GetGuild(String name) {
+				MySqlCommand cmd = CreateQuery();
+				cmd.CommandText = "SELECT * FROM guilds WHERE name = @n";
+				cmd.Parameters.AddWithValue("@n", name);
+		        GuildStruct[] guild;
+		        MySqlDataReader rdr = cmd.ExecuteReader();
+		        if (!rdr.HasRows) {
+		            return null;
+		        }
+		        
+		        rdr.Read();
+		        
+		        Object Id;
+				Object idUnknown;
+				Object nameUnknown;
+				Object Name;
+				Object Level;
+				Object Members;
+				Object GuildFame;
+				Object guildFameUnknown;
+				Object totalGuildFameUnknown;
+				Object TotalGuildFame;
+				Object levelUnknown;
+				Object Unknown;
+				Object membersUnknown;
+		        guild = new GuildStruct[] {
+		                Id=rdr.GetInt32(idUnknown,
+		                Name=rdr.GetString(nameUnknown,
+		                Level=rdr.GetInt32(levelUnknown,
+		                Members=rdr.GetString(membersUnknown.Split(,Unknown,
+		                GuildFame=rdr.GetInt32(guildFameUnknown,
+		                TotalGuildFame=rdr.GetInt32(totalGuildFameUnknown};
+		        return guild;
+		    }
+			}
     	 
      }
     	 
